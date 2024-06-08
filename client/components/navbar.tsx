890168fe61300187917c0e29cb5cc34909022d3c
tkddnr1022 <tkddnr1022@naver.com>
2024-06-08 18:25:04 +0900

    update background color

--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -42,7 +42,7 @@ export default function Navbar() {
   }
 
   return (
-    <Disclosure as="nav" className={navigation[0].current ? "absolute w-full z-50" : "bg-gray-800"}>
+    <Disclosure as="nav" className={navigation[0].current ? "absolute w-full z-50" : ""}>
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
